2bc57c7a44b82d415b41be0d49ad1dfc45a94c5c
George T Kramer <george.t.kramer@intel.com>
2019-06-25 16:48:00 -0700

    Fix up koji NFS server deployment
    Only install nfs-utils bundle.  This package was previously broken and
    relied on a dependency in storage-utils, but this has since been fixed.
    
    Add no_root_squash option for exporting a NFS mount to increase the read
    performance.
    
    Signed-off-by: George T Kramer <george.t.kramer@intel.com>

--- koji-setup/deploy-koji-nfs-server.sh
+++ koji-setup/deploy-koji-nfs-server.sh
@@ -7,10 +7,10 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 source "$SCRIPT_DIR"/globals.sh
 source "$SCRIPT_DIR"/parameters.sh
 
-swupd bundle-add storage-utils
+swupd bundle-add nfs-utils
 
 # Export server directory to be mounted by clients
-echo "$KOJI_DIR $KOJI_SLAVE_FQDN(ro)" >> /etc/exports
+echo "$KOJI_DIR $KOJI_SLAVE_FQDN(ro,no_root_squash)" >> /etc/exports
 
 systemctl enable --now rpcbind
 systemctl enable --now nfs-server
